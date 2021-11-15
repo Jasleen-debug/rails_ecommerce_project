5beebde6b2ba5eccd9018de44196d7fe51e81aa4
Jasleen <jasleen0859@gmail.com>
2021-11-15 14:47:32 -0600

    Added controllers and views for the items and category models

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  # get "food_items/index"
+  # get "food_items/show"
+  # get "food_categories/index"
+  # get "food_categories/show"
+
+  resources :food_items, only: %i[index show]
+  root to: "food_items#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
