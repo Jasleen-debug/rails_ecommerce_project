4120821db2717419ec88467e354f130056ddb67d
Jasleen <jasleen0859@gmail.com>
2021-11-15 23:11:44 -0600

    Added index and show pages for food categories

--- app/controllers/food_categories_controller.rb
+++ app/controllers/food_categories_controller.rb
@@ -1,7 +1,9 @@
 class FoodCategoriesController < ApplicationController
   def index
+    @food_categories = FoodCategory.all
   end
 
   def show
+    @food_category = FoodCategory.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,15 @@ Rails.application.routes.draw do
   # get "food_items/show"
   # get "food_categories/index"
   # get "food_categories/show"
-
+  resources :food_categories, only: %i[index show]
   resources :food_items, only: %i[index show]
+
+  resources :food_items, only: %i[index show] do
+    collection do
+      get "search"
+    end
+  end
+
   root to: "food_items#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
