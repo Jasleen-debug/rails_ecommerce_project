bb01642c73a0467f1013be255b4e07fd6acda1b0
Jasleen <jasleen0859@gmail.com>
2021-11-17 20:08:46 -0600

    Implemented category search on food items

--- app/controllers/food_items_controller.rb
+++ app/controllers/food_items_controller.rb
@@ -8,6 +8,12 @@ class FoodItemsController < ApplicationController
   end
 
   def search
-    @food_items = FoodItem.search(params[:keywords])
+    # @food_items = FoodItem.search(params[:keywords])
+    @food_items = if params[:food_category_id].to_s == ""
+                    FoodItem.where("name LIKE ?", "%#{params[:search_term]}%")
+                  else
+                    FoodItem.where("food_category_id = ? AND name LIKE ?", params[:food_category_id].to_s,
+                                   "%#{params[:search_term]}%")
+                  end
   end
 end
