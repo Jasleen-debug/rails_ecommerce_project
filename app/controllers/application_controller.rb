55fcf300a29e7af54f42783597300105318fd46c
Jasleen <jasleen0859@gmail.com>
2021-11-20 15:41:02 -0600

    Added province id to Users table through foreign key ref

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:province_id])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:province_id])
+  end
+
 end
---
+++ db/migrate/20211120205524_add_provinces_to_users.rb
@@ -0,0 +1,5 @@
+class AddProvincesToUsers < ActiveRecord::Migration[6.1]
+  def change
+    add_column :users, :province_id, :integer
+  end
+end
